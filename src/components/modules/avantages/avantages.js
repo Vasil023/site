a33f3114e225183355e2af485422482a217e9197
Vasil Voloshin <voloshin017@gmail.com>
2023-09-21 16:39:29 +0300

    fix: fix home page v2

--- src/components/modules/avantages/avantages.js
+++ src/components/modules/avantages/avantages.js
@@ -1,13 +1,13 @@
-modules.define('avantages', ['i-bem-dom'], function(provide, bemDom) {
+let loadMoreBtn = document.querySelector("#load-more-btn"),
+  items = document.querySelector(".load-more-block"),
+  img = document.querySelector(".avanteges-img"),
+  wrapper = document.querySelector(".avanteges-wrapper");
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+loadMoreBtn.onclick = (e) => {
+  e.preventDefault();
+  items.style.display = "block";
+  img.style.height = "857px";
+  wrapper.style.height = "857px";
 
-});
+  loadMoreBtn.style.display = "none";
+};
